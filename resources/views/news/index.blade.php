91af0e3b4f317bc6956ec09f9ddc98eb741c888b
長崎宏太朗 <kmny03@gmail.com>
2021-09-01 15:50:49 +0000

    app/Http/Controllers/Admin/NewsController.php,resources/views/news/index.blade.phpの編集

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -11,7 +11,7 @@
                             <div class="caption mx-auto">
                                 <div class="image">
                                     @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
+                                        <img src="{{ $headline->image_path }}">
                                     @endif
                                 </div>
                                 <div class="title p-2">
@@ -22,7 +22,6 @@
                         <div class="col-md-6">
                             <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
                         </div>
-                        <p>PHP/Laravelコース - Herokuへのデプロイ</p>
                     </div>
                 </div>
             </div>
@@ -46,7 +45,7 @@
                             </div>
                             <div class="image col-md-6 text-right mt-4">
                                 @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
+                                    <img src="{{ $post->image_path }}">
                                 @endif
                             </div>
                         </div>
